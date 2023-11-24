cd7e50275e39537403bc948752e3879be83910a2
Wei <prctjliwei@hotmail.com>
2023-11-24 17:26:07 -0500

    add more wait_for_db function

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,15 +2,19 @@
 """
 
 from django.test import SimpleTestCase
-from  app import calc
+from app import calc
+
 
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
     def test_add_numbers(self):
         """test adding numbers"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
         self.assertEqual(11, res)
-    
+        
+
     def test_subtract_numbers(self):
         """Test subtracting numbers"""
-        res = calc.subtract(10,15)
+        res = calc.subtract(10, 15)
+        self.assertEqual(5, res)
+      
