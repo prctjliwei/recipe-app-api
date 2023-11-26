ffc4b9b695909feaea14bb15512b2c64a0c7ca96
Wei <prctjliwei@hotmail.com>
2023-11-26 16:22:49 -0500

    feat: add user functionality

--- app/core/models.py
+++ app/core/models.py
@@ -1,1 +1,48 @@
-# Create your models here.
+"""
+Database models
+"""
+
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """create, save and return a new user"""
+        if not email:
+            raise ValueError("User must have an email address")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    # a superuser is created with email: admin@example.com, pwd=1234
+    def create_superuser(self, email, password):
+        """create, save and return a new super user"""
+        if not email:
+            raise ValueError("User must have an email address")
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
